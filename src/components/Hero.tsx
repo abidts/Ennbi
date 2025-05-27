c8e78e0c43baf9f556b2e8ad04c690c76af87853
abidts <128727652+abidts@users.noreply.github.com>
2025-05-28 01:12:43 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero: React.FC = () => {
           {/* Luxury Grey Title */}
           <motion.h1
             className="text-4xl md:text-7xl font-extrabold leading-[1.1] pb-4"
-            style={{ fontFamily: "'Major Mono Display', monospace" }}
+style={{ fontFamily: "'Share Tech Mono', monospace" }}
 
 
  // Artistic font added
